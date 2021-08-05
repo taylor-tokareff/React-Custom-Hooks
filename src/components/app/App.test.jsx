563daa4ad56d4c9fbe76158aa98750c4ec88660f
taylor-tokareff <taylortokareff@gmail.com>
2021-08-05 12:31:29 -0700

    tried to test

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('App component', () => {
-  afterEach(() => cleanup());
-  it('renders App', () => {
-    const { asFragment } = render(<App />);
-    expect(asFragment()).toMatchSnapshot();
+  it('displays a list of characters on the home page', async () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    screen.getByText('Loading...');
+
+    const ul = await screen.findByRole('list');
+    expect(ul).not.toBeEmptyDOMElement();
+  });
+
+  it('displays a characters details', async () => {
+    render(
+      <MemoryRouter initialEntries={['/3']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    screen.getByText('Loading...');
+
+    await screen.findByText('Bender');
+    await screen.findByText('I\'m a fraud.A poor, lazy, sexy, fraud.');
+    await screen.findByAltText('Bender');
   });
 });
